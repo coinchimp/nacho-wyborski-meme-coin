2054eb04c5b00cbf26bf490ce019b2c73d80e1d0
coinchimp <pinrojas+crypto@gmail.com>
2024-05-26 14:11:43 -0500

    changing the name

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,8 @@ import './globals.css'
 import Image from 'next/image'
 
 export const metadata = {
-  title: 'Nacho Wyborski KRC20 Meme Coin',
-  description: 'Nacho Wyborski KRC20 Meme Coin',
+  title: 'Nacho the 𐤊at KRC20 Meme Coin',
+  description: 'Nacho the 𐤊at KRC20 Meme Coin',
 }
 
 export default function RootLayout({ children }) {
@@ -12,12 +12,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <title>Nacho Wyborski KRC20 Meme Coin</title>
-        <meta name="description" content="Nacho Wyborski KRC20 Meme Coin" />
+        <title>Nacho the 𐤊at KRC20 Meme Coin</title>
+        <meta name="description" content="Nacho the 𐤊at KRC20 Meme Coin" />
       </head>
       <body>
         <div className="top-bar">
-          <Image src="/kaspa_logo_nacho.png" alt="Nacho Wyborski Logo" width={40} height={40} />
+          <Image src="/kaspa_logo_nacho.png" alt="Nacho the 𐤊at Logo" width={40} height={40} />
           <div className="icon-container">
             <a href="https://discord.gg/f3pUgRFx" target="_blank" rel="noopener noreferrer">
               <Image src="/icons/discord.png" alt="Discord" width={30} height={30} />
--- src/app/page.js
+++ src/app/page.js
@@ -7,24 +7,26 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Nacho Wyborski KRC20 Meme Coin</title>
-        <meta name="description" content="Nacho Wyborski KRC20 Meme Coin" />
+        <title>Nacho the 𐤊at KRC20 Meme Coin</title>
+        <meta name="description" content="Nacho the 𐤊at KRC20 Meme Coin" />
         <link rel="icon" href="/kaspa_logo_nacho.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Image src="/kaspa_logo_nacho.png" alt="Nacho Wyborski Logo" width={150} height={150} className={styles.logo} />
+        <Image src="/kaspa_logo_nacho.png" alt="Nacho the 𐤊at Logo" width={150} height={150} className={styles.logo} />
         <h1 className={styles.title}>
-          Nacho Wyborski KRC20 Meme Coin
-        </h1>
+          Nacho the 𐤊at
+        </h1>      
+        <h2 className={styles.title2}>    
+          KRC20 Meme Coin<br/>
+          coming soon!
+        </h2>
         <p className={styles.description}>
+          <br/>
           fair launch<br/>
           no presale<br/>
           no pre-allocation<br/>
-        </p>
-        <h2 className={styles.title}>
-          coming soon!
-        </h2>        
+        </p>      
       </main>
     </div>
   )
