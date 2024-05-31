c257798b6af99d0a46a788dba55a06cb44d42819
coinchimp <pinrojas+crypto@gmail.com>
2024-05-31 09:30:38 -0500

    moving img to Image and spacing characters in FAQ

--- src/app/Banner.js
+++ src/app/Banner.js
@@ -1,11 +1,10 @@
 // src/app/Banner.js
 import styles from './Banner.module.css';
-import Image from 'next/image';
 
 const Banner = () => {
   return (
     <div className={styles.banner}>
-      <Image src="/nacho_banner.jpg" alt="Banner" className={styles.bannerImage} />
+      <img src="/nacho_banner.jpg" alt="Banner" className={styles.bannerImage} />
     </div>
   );
 };
